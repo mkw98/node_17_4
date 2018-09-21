b5a843059ef36311389069a14be5d80eccd7a071
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-22 01:19:46 +0200

    Create time.js

---
+++ modules/time.js
@@ -0,0 +1,4 @@
+//Stwórz moduł, który będzie poprawnie formatował czas. Zależy nam na napisaniu funkcji:
+//do przekształcania sekund na minuty - przykładowo, podając 125 sekund wyświetli 2 min. 5 sek.
+//sekund na godziny - podając 3700 sekund wyświetli 1 godz. 1 min. 40 sek.
+//dodaj moduł do katalogu modules i skorzystaj z niego w module OSinfo do poprawnego formatowania czasu działania systemu
