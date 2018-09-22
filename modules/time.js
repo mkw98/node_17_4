854977c6c4af48311a80a7c2cc6925adceb85ec9
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-22 22:09:12 +0200

    Update time.js

--- modules/time.js
+++ modules/time.js
@@ -1,3 +1,17 @@
+var hours;
+var minutes;
+var seconds;
+
+var hours = uptime / 60;
+var minutes = (uptime - (hours * 3600)) / 60;
+var seconds = uptime - (hours * 3600) - (minutes * 60);
+
+//function formatUptime(uptime) {
+//  if (uptime < 60) {
+//    return `${uptime} sek.`;
+//  }
+//}
+
 //Stwórz moduł, który będzie poprawnie formatował czas. Zależy nam na napisaniu funkcji:
 //do przekształcania sekund na minuty - przykładowo, podając 125 sekund wyświetli 2 min. 5 sek.
 //sekund na godziny - podając 3700 sekund wyświetli 1 godz. 1 min. 40 sek.
